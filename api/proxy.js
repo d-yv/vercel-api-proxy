5d9a54e92d21704a03aaad68aac219793ef9976a
d-yv <yuri.dubrovin@gmail.com>
2025-06-18 18:19:32 +0300

    add structure

---
+++ api/proxy.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const API_TOKEN = process.env.GETIP_API_TOKEN;
+  const ip = req.query.q;
+
+  try {
+    const response = await fetch(`https://ipinfo.io/${ip}?`, {
+      headers: {
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Ошибка при запросе к API" });
+  }
+}
